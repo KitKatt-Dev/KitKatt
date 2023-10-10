5778e896a4c79e8e65b72ccc2f28daa197df031c
KitKatt-Dev <147453971+KitKatt-Dev@users.noreply.github.com>
2023-10-09 20:47:31 -0400

    Update main.py v0.010
    I'm updating this script when I have the time to do so, it's 8:45 pm (EST), imma have time to do so is able to update the bot and this page.

--- main.py
+++ main.py
@@ -47,16 +47,25 @@ async def say(ctx, *, message):
 async def purge(ctx, amt):
   await ctx.channel.purge(limit=int(amt) + 1)
   msg = await ctx.send(f"Purged {amt} messages. Used by @{ctx.author.name}")
-  await asyncio.sleep(1) 
+  await asyncio.sleep(1)
   await msg.delete()
 
 
 @bot.command()
 async def source(ctx):
   await ctx.reply(
-      "# KitKatt Source Code, ## \n https://github.com/KitKatt-Dev/KitKatt")
+      "# KitKatt Source Code ## \n https://github.com/KitKatt-Dev/KitKatt")
+  await ctx.message.delete()
+
+
+@bot.command()
+async def commands(ctx, ):
+  await ctx.reply(
+      "# KitKatt Commands ## \n /say - You can maake it say what you want, but beware staff can see who did it.  \n /purge - (Staff Only) Deletes messages up to 2 weeks prior to use.  \n /source - Gives the link to the source code. \n /ping - KitKatt says Pong! "
+  )
   await ctx.message.delete()
 
 
 keep_alive()
-bot.run('MTE1MzY3NzA0NjQ4MDg5NjEwMg.GIPv6S.fx05zMymcpmh8ckmkLVq9Z83wWf8p7QaAWvRSk')
+bot.run(
+  'YOUR TOKEN HERE')
